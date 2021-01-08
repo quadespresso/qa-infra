04300b21f23a71894e9252dc7b660f806baadd4f
Jim Carroll <pcarroll@mirantis.com>
2021-01-08 12:43:54 -0600

    Cumulative updates to Terraform config
    * check validity of AWS credentials, since terraform doesn't fail this very well
    * change UCP refs to MKE
    * change DTR refs to MSR
    * comment out explicit references to private interfaces
    * bump subnets from 2 to 3 -- prep for larger clusters
    * new platforms:
      * CentOS 8.2/8.3
      * Oracle Linux 7.9/8.1/8.2/8.3

--- modules/manager/outputs.tf
+++ modules/manager/outputs.tf
@@ -1,15 +1,20 @@
 output "lb_dns_name" {
-  value = aws_lb.ucp_manager.dns_name
+  value = aws_lb.mke_manager.dns_name
 }
 
 output "public_ips" {
-  value = aws_instance.ucp_manager.*.public_ip
+  value = aws_instance.mke_manager.*.public_ip
 }
 
 output "private_ips" {
-  value = aws_instance.ucp_manager.*.private_ip
+  value = aws_instance.mke_manager.*.private_ip
 }
 
 output "machines" {
+<<<<<<< HEAD:system_test_toolbox/launchpad/modules/manager/outputs.tf
   value = aws_instance.ucp_manager
 }
+=======
+  value = aws_instance.mke_manager
+}
+>>>>>>> f7fcff6... Cumulative updates to Terraform config:launchpad/modules/manager/outputs.tf
