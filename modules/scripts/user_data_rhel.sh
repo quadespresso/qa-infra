b9026b4b08bba0bec56f3a45e9717617803ed5be
Jim Carroll <pcarroll@mirantis.com>
2022-04-07 11:00:25 -0500

    Add provisioner configs for RHEL 8.5 (#795)
    * also add RHEL 8.5 to launchpad platforms map
    * disable RHEL subscription manager

--- modules/scripts/user_data_rhel.sh
+++ modules/scripts/user_data_rhel.sh
@@ -4,5 +4,10 @@
 
 # RHEL customization
 
+# Disable subscription manager on applicable systems
+# https://sahlitech.com/entitlement-server-fix/
+SUBMGRCONF="/etc/yum/pluginconf.d/subscription-manager.conf"
+[ -f $SUBMGRCONF ] && sed -i -e "s|^enabled=.*|enabled=0|" $SUBMGRCONF
+
 # Prep for NFS mount (MSR 2.x prerequisites)
 yum install -y nfs-utils
